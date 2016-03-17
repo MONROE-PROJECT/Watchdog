e168a2e4494671c494e6175400157adc43112f13
Thomas Hirsch <thomas.hirsch@celerway.com>
2016-03-17 08:45:42 +0100

    enable status leds via biteback

--- biteback/biteback.py
+++ biteback/biteback.py
@@ -16,6 +16,8 @@ SYSEVENT_STATUS   = "Watchdog.Status"
 tests = register.get()
 context = zmq.Context()
 
+leds_enabled=False
+
 def sysevent(message, eventType = SYSEVENT_FAILED):
     try:
         socket = context.socket(zmq.REQ)
@@ -33,6 +35,17 @@ def succeeds(method):
         print "[DEBUG2]",ex
         return False
 
+def init_leds():
+    ok = shell("modprobe leds-apu; echo $?").strip() 
+    if ok == "0":
+        return True
+    return False
+
+def leds(a,b,c):
+    if not leds_enabled: return
+    shell("echo %i > /sys/class/leds/apu\:%i/brightness" % (1 if a else 0, 1))
+    shell("echo %i > /sys/class/leds/apu\:%i/brightness" % (1 if b else 0, 2))
+    shell("echo %i > /sys/class/leds/apu\:%i/brightness" % (1 if c else 0, 3))
 
 def watchdog(doRepairs=True, doFinals=True):
     print "Imported %i tests" % len(tests)
@@ -62,13 +75,20 @@ def watchdog(doRepairs=True, doFinals=True):
         run += 1
 
     sysevent("%i/%i tests succeeded, %i tests run." % (success, len(tests), run), SYSEVENT_STATUS)
+    if success == len(tests):
+        leds(True, True, True)
+    else:
+        leds(False, False, False)
+
 
 def main():
+    global leds_enabled
     parser = argparse.ArgumentParser()
     parser.add_argument("-R","--skip-repairs", help="do not run repair actions", action="store_true")
     parser.add_argument("-F","--skip-finals", help="do not run final actions", action="store_true")
     args = parser.parse_args()
 
+    leds_enabled = init_leds()
     watchdog(not args.skip_repairs, not args.skip_finals)
 
 if __name__ == "__main__":
