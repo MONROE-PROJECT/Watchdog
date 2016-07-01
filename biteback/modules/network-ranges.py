0e93c8997b1cf18fde68ea219837d50acf59033e
Thomas Hirsch <thomas.hirsch@celerway.com>
2016-07-01 15:28:10 +0200

    sync executable names

--- biteback/modules/network-ranges.py
+++ biteback/modules/network-ranges.py
@@ -16,7 +16,7 @@ class AssignNetworkRange:
             target = "192.168."+str(id)
             ip = shell ("ip -f inet -o addr show "+ifname+" 2>/dev/null | sed -e 's|.*inet \\(.*\\)/.*|\\1|g'").strip()
             if ip and not target in ip:
-                shell ("zte-iprange "+ifname+" "+target, timeout=20)
+                shell ("mf910-iprange "+ifname+" "+target, timeout=20)
 
 class Ranges (module.BasicModule):
     """Check that mifi network range matches interface name"""
