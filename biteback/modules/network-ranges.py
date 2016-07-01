c5088b20d4cc0902123b51a5c63b30981de28aa4
Thomas Hirsch <thomas.hirsch@celerway.com>
2016-07-01 15:54:27 +0200

    [fix] off by one

--- biteback/modules/network-ranges.py
+++ biteback/modules/network-ranges.py
@@ -11,7 +11,7 @@ class RangesFinal:
 class AssignNetworkRange:
     """assign network range to mifi"""
     def run(self):
-        for id in xrange(0,2):
+        for id in xrange(0,3):
             ifname = "usb"+str(id)
             target = "192.168."+str(id)
             ip = shell ("ip -f inet -o addr show "+ifname+" 2>/dev/null | sed -e 's|.*inet \\(.*\\)/.*|\\1|g'").strip()
@@ -25,7 +25,7 @@ class Ranges (module.BasicModule):
     final   = RangesFinal()
 
     def run(self):
-        for id in xrange(0,2):
+        for id in xrange(0,3):
             ifname = "usb"+str(id)
             target = "192.168."+str(id)
             ip = shell ("ip -f inet -o addr show "+ifname+" 2>/dev/null | sed -e 's|.*inet \\(.*\\)/.*|\\1|g'").strip()
