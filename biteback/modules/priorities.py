4329db87f6b0c5fd1b1cf20d4b1a3182f078ead9
Thomas Hirsch <thomas.hirsch@celerway.com>
2016-07-12 11:53:02 +0200

    set dlb priorities for management links

---
+++ biteback/modules/priorities.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from biteback import module, register
+from biteback.util import shell, trigger_maintenance
+import sqlite3 as db
+
+class PrioritiesFinal:
+    """Maintenance"""
+    def run(self):
+        return trigger_maintenance()
+
+class Priorities(module.BasicModule):
+    """Correct priorities are set for load balancer interfaces"""
+
+    repairs = []
+    final   = PrioritiesFinal()
+
+    # from dlb/Address.h
+    PRIO_04MB   = 2
+    PRIO_100MB  = 11
+    PRIO_500MB  = 13
+    PRIO_1000MB = 14
+
+    def run(self):
+        conn = db.connect('/etc/config/celerway.db')
+        c = conn.cursor()
+        links = c.execute("SELECT ICCIDMAC, USEREST from Links")
+
+        eth0 = shell("cat /sys/class/net/eth0/address 2>/dev/null").replace(":","").strip()
+        wlan0 = shell("cat /sys/class/net/wlan0/address 2>/dev/null").replace(":","").strip()
+        wwan0 = shell("cat /sys/class/net/wwan0/address 2>/dev/null").replace(":","").strip()
+
+        priorities = {
+            eth0:  self.PRIO_1000MB,
+            wlan0: self.PRIO_500MB,
+            wwan0: self.PRIO_100MB
+        }
+
+        for mac,userest in links:
+            if mac in priorities:
+                if userest != priorities[mac]:
+                    c.execute("UPDATE Links SET USEREST=? WHERE ICCIDMAC=?", (priorities[mac], mac))
+            else:
+                c.execute("UPDATE Links SET USEREST=? WHERE ICCIDMAC=?", (self.PRIO_04MB, mac))
+        conn.commit()
+        conn.close()
+        return True
+
+register.put(Priorities())
