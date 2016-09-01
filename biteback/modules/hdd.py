5c430d0d7aec9b86345430a9568a0564743f3de2
Thomas Hirsch <thomas.hirsch@celerway.com>
2016-09-01 10:10:24 +0200

    clean docker/devicemapper on disk full

--- biteback/modules/hdd.py
+++ biteback/modules/hdd.py
@@ -15,15 +15,23 @@ class ClearLogs:
         shell("rm /var/log/*.gz", timeout=60)
         shell("apt-get clean", timeout=120)
 
+class RmDocker:
+    """delete /var/lib/docker and reinstall docker-engine"""
+    def run(self):
+        shell("docker stop -t 0 $(docker ps -q)", timeout=60)
+        shell("systemctl stop docker", timeout=60)
+        shell("rm -r /var/lib/docker", timeout=60)
+        shell("apt-get remove docker-engine", timeout=120)
+
 class HddUsage (module.BasicModule):
     """Disk space available"""
 
-    repairs = [ClearLogs()]
+    repairs = [ClearLogs(), RmDocker()]
     final   = HddFinal()
 
     def run(self):
         hddleft =  int(shell("df / --output=avail|tail -n1"))
-        if hddleft < 2000000:
+        if hddleft < 500000:
             return False
         return True
 
