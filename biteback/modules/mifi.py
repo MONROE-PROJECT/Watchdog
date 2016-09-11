4077f90818272eae4a38a71ad7803a67df6b17b5
Thomas Hirsch <thomas.hirsch@celerway.com>
2016-09-11 22:27:21 +0200

    modes

--- biteback/modules/mifi.py
+++ biteback/modules/mifi.py
@@ -25,7 +25,7 @@ class Mifi (module.BasicModule):
     def run(self):
         modems = json.loads(shell("curl -s http://localhost:88/modems"))
         for modem in modems:
-            if modem.get('mode') in ['LTE','4G']:
+            if modem.get('mode') in ['LTE','4G','UMTS']:
                 interface=modem.get('ifname')
                 ip4table=modem.get('ip4table')
                 code = shell('curl -4 --interface '+interface+' -sL -w "%{http_code}" http://193.10.227.25/test/1000M.zip -r 0-0 -o /dev/null --connect-timeout 10 -m 20; echo " $?"', timeout=30).strip()
