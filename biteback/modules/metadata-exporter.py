27f39686a57542f254ba57f6b0d8f06c9497b33b
Thomas Hirsch <thomas.hirsch@celerway.no>
2017-04-13 13:25:06 +0200

    do not run metadata-exporter tests if interface does not exist

--- biteback/modules/metadata-exporter.py
+++ biteback/modules/metadata-exporter.py
@@ -35,6 +35,11 @@ class MEService (module.BasicModule):
     final   = MEFinal()
 
     def run(self):
+        # if the docker0 interface does not exist, not connecting is ok
+        docker = shell("ifconfig docker0 2>/dev/null | grep inet | grep ask")
+        if not "inet" in docker:
+            return True
+
         ps =  shell("ps ax|grep exporter")
         if not "metadata-exporter" in ps: 
             return False
@@ -43,10 +48,6 @@ class MEService (module.BasicModule):
         if not "running" in status: 
             return False
 
-        # if the docker0 interface does not exist, not connecting is ok
-        docker = shell("ifconfig docker0 2>/dev/null | grep inet | grep ask")
-        if not "inet" in docker:
-            return True
 
         print("Subscribing to ZMQ socket on tcp://172.17.0.1:5556")
         context = zmq.Context()
