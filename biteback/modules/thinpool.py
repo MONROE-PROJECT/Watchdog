85c8fac0af63292519da7920cecbd181b4199cc3
Thomas Hirsch <thomas.hirsch@celerway.no>
2017-05-25 08:19:47 +0200

    thinpool sync fixes

---
+++ biteback/modules/thinpool.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from biteback import module, register
+from biteback.util import shell, trigger_maintenance
+
+class ThinpoolFinal:
+    """Maintenance"""
+
+    def run(self):
+        return trigger_maintenance("Thinpool device out of sync. Removed, to be restored by ansible.")
+
+class RemoveThinpool:
+    """remove thinpool device and stop docker daemon"""
+
+    def run(self):
+        shell("systemctl stop docker", timeout=60)                                       # should be stopped, just in case 
+        shell("dmsetup ls|grep docker-|cut -f1 -d'('|xargs dmsetup remove", timeout=60)  # remove any stale leases on the thinpool
+        shell("lvremove -f /dev/mapper/vg--monroe-tp--docker", timeout=60)               # remove the thinpool device
+        shell("systemctl start docker")                                                  # will fail, but remove the systemctl status message this test triggers on
+
+class Thinpool (module.BasicModule):
+    """docker can access thinpool device"""
+
+    repairs = [RemoveThinpool()]
+    final   = ThinpoolFinal()
+
+    def run(self):
+        status = shell("systemctl status docker -l")
+        if "Unable to take ownership of thin-pool" in status:
+            return False
+        return True
+
+register.put(Thinpool())
