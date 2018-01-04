2051fbc974f51386503721e514fce30404dd4680
Thomas Hirsch <thomas.hirsch@celerway.com>
2018-01-04 13:57:43 +0100

    check for accumulating metadata files in /tmp

--- biteback/modules/hdd.py
+++ biteback/modules/hdd.py
@@ -42,6 +42,9 @@ class HddUsage (module.BasicModule):
         hddleft =  int(shell("df /var/log --output=avail|tail -n1"))
         if hddleft < 10000:
             return False
+        files =  int(shell("ls /tmp | wc -l"))
+        if files > 1000:
+            return False
         return True
 
 register.put(HddUsage())
